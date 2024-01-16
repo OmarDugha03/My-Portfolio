707b2100ce2d3f1646fa794377a93a5dc9673254
OmarDugha <116832203+OmarDugha03@users.noreply.github.com>
2024-01-16 16:12:42 +0300

    Style : Add the main images to the APp

---
+++ public/index.ts
@@ -0,0 +1,5 @@
+
+export {default as logo} from "./myLogo.svg"
+export {default as sfw} from "./software.png"
+export {default as bulb} from "./bulb.png"
+export {default as frontend} from "./frontend.png"
